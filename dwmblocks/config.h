8ac87b67e9123f689bc5112e0647641506dd3438
bwkam <userdev987@gmail.com>
2024-05-05 14:30:06 +0300

    latest

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -16,17 +16,19 @@
 // Control whether a trailing delimiter should be appended to the status.
 #define TRAILING_DELIMITER 0
 
+#define TOSTR(n) #n 
+
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+       X("", TOSTR(SCRIPTS_DIR)"/date", 1, 10)  \
+    // X("", "sb-music", 0, 2)   \
+    // X("", "sb-disk", 1800, 3) \
+    // X("", "sb-memory", 10, 4) \
+    // X("", "sb-loadavg", 5, 5) \
+    // X("", "sb-mic", 0, 6)     \
+    // X("", "sb-record", 0, 7)  \
+    // X("", "sb-volume", 0, 8)  \
+    // X("", "sb-battery", 5, 9) \
+    // X("", "sb-date", 1, 10)
 
 #endif  // CONFIG_H
