055ce4c99ca10642661d9a5640732271be67a032
bwkam <userdev987@gmail.com>
2024-05-05 13:17:29 +0300

    modify makefile to install scripts

--- dwmblocks.nix
+++ dwmblocks.nix
@@ -1,6 +1,8 @@
-{ stdenv, xorg, libX11, pkg-config }:
+{ stdenv, xorg, libX11, pkg-config, makeWrapper, lib, coreutils }:
 
-stdenv.mkDerivation {
+let inherit (lib) makeBinPath;
+
+in stdenv.mkDerivation {
   pname = "dwmblocks";
   version = "4.20.24";
 
@@ -8,9 +10,15 @@ stdenv.mkDerivation {
 
   strictDeps = true;
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ pkg-config makeWrapper ];
   buildInputs = [ libX11 ]
     ++ builtins.attrValues { inherit (xorg) libxcb xcbutil xlsatoms xcbproto; };
+
+  postInstall = ''
+    patchShebangs --host $out/scripts
+    wrapProgram $out/scripts/date --prefix PATH ":" \
+      "${makeBinPath [ coreutils ]}"
+  '';
 
   makeFlags = [ "PREFIX=$(out)" ];
 
