f6573742cc062917fc3457c39bfe727d7725fe6e
bwkam <userdev987@gmail.com>
2024-05-07 00:50:19 +0300

    add date script

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {
-    "Iosevka Nerd Font:size=12:style=Regular",
+    "Iosevka Nerd Font:size=10:style=Regular",
     "Material Design Icons Desktop:size=11"
 };
 static const char dmenufont[] = "monospace:size=10";
