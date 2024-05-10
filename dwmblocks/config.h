6a9e55d2eafba98fd8250488a8e0b9af0665996d
bwkam <userdev987@gmail.com>
2024-05-10 11:45:04 +0300

    change order  of blocks

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -19,8 +19,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-       X("", "SCRIPTS_DIR/bat", 1, 0)   \
        X("", "SCRIPTS_DIR/date", 1, 0)  \
+       X("", "SCRIPTS_DIR/bat", 1, 0)   \
     // X("", "sb-disk", 1800, 3) \
     // X("", "sb-memory", 10, 4) \
     // X("", "sb-loadavg", 5, 5) \
