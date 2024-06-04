baeffa209c91b2fafffad9f0bf0b5c4804e1dc7c
bwkam <userdev987@gmail.com>
2024-06-04 22:19:09 +0300

    decrease active opacity

--- dwm/config.h
+++ dwm/config.h
@@ -18,7 +18,7 @@ static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
 
 static const double activeopacity =
-    0.9f; /* Window opacity when it's focused (0 <= opacity <= 1) */
+    0.8f; /* Window opacity when it's focused (0 <= opacity <= 1) */
 static const double inactiveopacity =
     0.7f; /* Window opacity when it's inactive (0 <= opacity <= 1) */
 
