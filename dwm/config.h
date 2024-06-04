2ab6d43159d8fbf6d55e54c2b166b22d841e68fe
bwkam <userdev987@gmail.com>
2024-06-04 21:36:29 +0300

    change bar to black

--- dwm/config.h
+++ dwm/config.h
@@ -3,32 +3,38 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
+static const unsigned int systraypinning =
+    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+          X */
+static const unsigned int systrayonleft =
+    0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst =
+    1; /* 1: if pinning fails, display systray on the first monitor, False:
+          display systray on the last monitor*/
+static const int showsystray = 1; /* 0 means no systray */
 
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const double activeopacity =
+    0.9f; /* Window opacity when it's focused (0 <= opacity <= 1) */
+static const double inactiveopacity =
+    0.7f; /* Window opacity when it's inactive (0 <= opacity <= 1) */
 
-static const double activeopacity   = 0.9f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
-static const double inactiveopacity = 0.7f;     /* Window opacity when it's inactive (0 <= opacity <= 1) */
-
-static const char *fonts[] = {
-    "Iosevka Nerd Font:size=10:style=Regular",
-    "Material Design Icons Desktop:size=11"
-};
+static const char *fonts[] = {"Iosevka Nerd Font:size=10:style=Regular",
+                              "Material Design Icons Desktop:size=11"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
+static const char col_black[] = "#000000";
+
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeNorm] = {col_gray3, col_black, col_gray2},
     [SchemeSel] = {col_gray4, col_cyan, col_cyan},
 };
 
@@ -40,16 +46,17 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-   /* class      instance    title       tags mask     isfloating   focusopacity    unfocusopacity     monitor */
-   { "Gimp",     NULL,       NULL,       0,            1,           1.0,            inactiveopacity,   -1 },
-   { "Firefox",  NULL,       NULL,       1 << 8,       0,           activeopacity,  inactiveopacity,   -1 },
+    /* class      instance    title       tags mask     isfloating focusopacity
+       unfocusopacity     monitor */
+    {"Gimp", NULL, NULL, 0, 1, 1.0, inactiveopacity, -1},
+    {"Firefox", NULL, NULL, 1 << 8, 0, activeopacity, inactiveopacity, -1},
 
 };
 
 /* layout(s) */
-static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints = 0;  /* 0 fixes the ff border bug thing */
+static const float mfact = 0.55;  /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;     /* number of clients in master area */
+static const int resizehints = 0; /* 0 fixes the ff border bug thing */
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
@@ -107,10 +114,10 @@ static const Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    { MODKEY|ShiftMask,             XK_a,      changefocusopacity,   {.f = +0.025}},
-    { MODKEY|ShiftMask,             XK_s,      changefocusopacity,   {.f = -0.025}},
-    { MODKEY|ShiftMask,             XK_z,      changeunfocusopacity, {.f = +0.025}},
-    { MODKEY|ShiftMask,             XK_x,      changeunfocusopacity, {.f = -0.025}},
+    {MODKEY | ShiftMask, XK_a, changefocusopacity, {.f = +0.025}},
+    {MODKEY | ShiftMask, XK_s, changefocusopacity, {.f = -0.025}},
+    {MODKEY | ShiftMask, XK_z, changeunfocusopacity, {.f = +0.025}},
+    {MODKEY | ShiftMask, XK_x, changeunfocusopacity, {.f = -0.025}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
