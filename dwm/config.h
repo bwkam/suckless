c7e466988fd0ea6095fdecf7a91c35c670be47e1
bwkam <userdev987@gmail.com>
2024-11-16 11:49:23 +0200

    nicer white

--- dwm/config.h
+++ dwm/config.h
@@ -31,7 +31,7 @@ static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#005577";
-static const char col_white[] = "#FFFFFF";
+static const char col_white[] = "#949191";
 static const char col_black[] = "#000000";
 
 static const char *colors[][3] = {
