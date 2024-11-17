1bd4b0109f83289305663163de88bceac91f8753
bwkam <userdev987@gmail.com>
2024-11-18 01:20:06 +0200

    dmenu: change selection bg

--- dwm/config.h
+++ dwm/config.h
@@ -113,8 +113,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_black,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont, "-nb", col_black, "-nf",
+    col_gray3,   "-sb", col_white, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"kitty", "--single-instance", NULL};
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = {"kitty",
